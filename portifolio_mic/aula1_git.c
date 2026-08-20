4ebe7728e746e98f08429407796ce1ebdb521a6a
Gabri3l-pm <gabrieldemossi21@gmail.com>
2026-08-20 14:13:31 +0000

    Aula 1 teste de upload

--- portifolio_mic/aula1_git.c
+++ portifolio_mic/aula1_git.c
@@ -7,15 +7,15 @@
 // #define F_CPU 16000000 || O Microship studio pede isso porém o compilador no Tails OS já está configurado para definir isso
 
 #include <avr/io.h> 	//Mapeamento dos registradores || No microship studio #include <xc.h>
+#include "util/delay.h"
 
-
-int main(void)
-{
+int main(void){
+	DDRB |= 0b00100000; //Define pino PB5 como saída
 	while(1) 	// Manter loop para microcontrolador funcionar repetidamente
 	{
-		//TODO
+		PORTB |= 0b00100000; //Atribui nível  lógico alto no pino PB5
+		_delay_ms(100);
+		PORTB &= 0b11011111; //Atríbui nivel lógico baixo ao pino PB5
+		_delay_ms(900);
 	}
 }
-
-//teste de commit
-
