97675990a1841d3175f18910cdfdc93ac4147e34
Gabri3l-pm <gabrieldemossi21@gmail.com>
2026-08-20 14:33:12 +0000

    Mudanças

--- portifolio_mic/aula1_git.c
+++ portifolio_mic/aula1_git.c
@@ -11,11 +11,14 @@
 
 int main(void){
 	DDRB |= 0b00100000; //Define pino PB5 como saída
+	DDRD &= 0b11110111; //Define pino PD3 como entrada
 	while(1) 	// Manter loop para microcontrolador funcionar repetidamente
 	{
-		PORTB |= 0b00100000; //Atribui nível  lógico alto no pino PB5
-		_delay_ms(100);
-		PORTB &= 0b11011111; //Atríbui nivel lógico baixo ao pino PB5
-		_delay_ms(900);
+		if((PIND & 0b00001000) != 0) { // Ler valor da chave + operador loǵico especificando a leitura apenas PD3, e segue se tiver nível lógico alto
+			PORTB |= 0b00100000; //Atribui nível  lógico alto no pino PB5
+			_delay_ms(100);
+			PORTB &= 0b11011111; //Atríbui nivel lógico baixo ao pino PB5
+			_delay_ms(900);
+		}
 	}
 }
