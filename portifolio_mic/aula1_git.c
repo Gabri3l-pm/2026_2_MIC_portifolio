21d303f190fc2db2891bccc78975af9ace323a9c
Gabri3l-pm <gabrieldemossi21@gmail.com>
2026-08-20 12:24:50 +0000

    Teste de commit, no Tail OS pelo terminal

---
+++ portifolio_mic/aula1_git.c
@@ -0,0 +1,19 @@
+/*
+*main.c
+*
+*Create: 20/08/2026 12:00:15 AM
+* Author: Gabriel Demossi P. Machado
+*/
+// #define F_CPU 16000000 || O Microship studio pede isso porém o compilador no Tails OS já está configurado para definir isso
+
+#include <avr/io.h> 	//Mapeamento dos registradores || No microship studio #include <xc.h>
+
+
+int main(void)
+{
+	while(1) 	// Manter loop para microcontrolador funcionar repetidamente
+	{
+		//TODO
+	}
+}
+
