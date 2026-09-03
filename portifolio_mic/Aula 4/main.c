c02a33a8a8962e9c8b9b8ca1c2ca21b21b365038
Gabri3l-pm <gabrieldemossi21@gmail.com>
2026-09-03 13:49:41 +0000

    Implementação de eventos para interupção e implementação de um resistor DIP na simulação

--- portifolio_mic/Aula 4/main.c
+++ portifolio_mic/Aula 4/main.c
@@ -4,25 +4,46 @@
 *Create: 08/06/2026 11:57:67 AM
 * Author: Gabriel Demossi P. Machado
 */
-//O Microship studio pede isso porém o compilador já foi previamente configurado
-#define F_CPU 16000000
+//O Microship studio pede isso porém o compilador já foi previamente configurado #define F_CPU 16000000
 #include <avr/io.h> 	//Mapeamento dos registradores || No microship studio #include <xc.h>
 #include "avr/interrupt.h"
-
-ISR(PCINT0_vect){
-
-}
+#include "util/delay.h"
 
 void GPIO_config(){
   //Seleciona os bits e os inverte para nível lógico baixo (Boa prática de código), 0b1111 0000
   DDRB &= ~((1<<DDB0)|(1<<DDB1)|(1<<DDB2)|(1<<DDB3)); 
-  DDRB = 0xff; //Seta todos os pinos como saída
-  //DDRB |= (1<<DDD0)|(1<<DDD1)|(1<<DDD2)|(1<<DDD3)(1<<DDD4)(1<<DDD5)(1<<DDD6)|(1<<DDD7);                    
+  PORTB |= (1<<DDB0)|(1<<DDB1)|(1<<DDB2)|(1<<DDB3); 
+  //DDRB = 0xff; Seta todos os pinos da porta D como saída 
+  DDRD |= (1<<DDD0)|(1<<DDD1)|(1<<DDD2)|(1<<DDD3)|(1<<DDD4)|(1<<DDD5)|(1<<DDD6)|(1<<DDD7);                    
+  DDRC |= (1<<DDC0); //Pino PC0 como saída
 }
+
+void GPIO_incBar(){
+  PORTD = PORTD >> 1;
+  PORTD |= 0b10000000; //Aciona o bit mais significativo D7
+}
+
+void PCINT_config() {
+  PCICR |= (1<<PCIE0); //Habilita grupo 0 de vetores
+  PCMSK0 |= (1<<PCINT3)|(1<<PCINT2)|(1<<PCINT1)|(1<<PCINT0); //habilita interrupção PCINT-PCIN
+}
+
+
+ISR(PCINT0_vect){
+  PORTC |= (1<<PORTC0); //seta pino PC0
+  _delay_ms(100);
+  PORTC &= ~(1<<PORTC0); //zera pino PC0
+  GPIO_incBar();
+}
+
 int main(void)
 {
-	while(1) 	// Nunca retira esse while, pois o programa depende dele para funcionar
-	{
-		//TODO
-	}
+  GPIO_config();
+  PCINT_config();
+  sei();
+  
+  while(1) 	// Nunca retira esse while, pois o programa depende dele para funcionar
+  {
+    //TODO
+  }
 }
