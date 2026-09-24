c01c3c0666a01ce1ff1dfa6c260a69b4ef359988
Gabri3l-pm <gabrieldemossi21@gmail.com>
2026-09-24 11:22:37 -0300

    Tratamento de recepção UART via interrupção

--- portifolio_mic/Aula 7/main.c
+++ portifolio_mic/Aula 7/main.c
@@ -9,13 +9,15 @@
 //#define F_CPU 16000000 
 #include <avr/io.h> 	//Mapeamento dos registradores 
 #include "util/delay.h"	
+#include "avr/interrupt.h"
 
 /**
  * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par, BAUD Variável
 **/
 void UART_config_rx(uint16_t pBAUND) {
 	UCSR0A = (0<<U2X0);							//Modo "double speed" desativado
-	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02); // Habilita o receptor UART0, frame de 8 bits
+	UCSR0B = (1<<RXEN0)|(0<<TXEN0)|(0<<UCSZ02) // Habilita o receptor UART0, frame de 8 bits
+        | (1<<RXCIE0);                          // Habilita interrupção do UART0
 	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)			// Modo assíncrono
 		   | (1<<UPM01)|(0<<UPM00)				// Habilita paridade par
 		   | (1<<USBS0)							// 2 bits de stop
@@ -27,11 +29,8 @@ void UART_config_rx(uint16_t pBAUND) {
 void GPIO_config(){
     DDRC = (1<<DDC0)|(1<<DDC1); //PC0 e PC1 como saída
 }
-int main(void){
-    GPIO_config();
-    UART_config_rx(9600);
-    while(1){
-        while((UCSR0A & (1<<RXC0)) == 0);
+
+ISR(USART_RX_vect) {
         uint8_t tReceivedByte = UDR0; //Leitura do Buffer
         //Tratamento de mmensagem recebida
         if(tReceivedByte == 'M') {
@@ -41,5 +40,12 @@ int main(void){
         }
         _delay_ms(1);
         PORTC = 0; //Apaga todos os LEDs
+}
+int main(void){
+    GPIO_config();
+    UART_config_rx(9600);
+    sei(); //Habilita interrupção globais
+    while(1){
+       
     }
 }
