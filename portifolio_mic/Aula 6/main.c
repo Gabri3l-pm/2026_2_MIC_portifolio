38399b4d9c395b044384efca3ada780a665b25c7
Gabri3l-pm <gabrieldemossi21@gmail.com>
2026-09-24 09:36:01 -0300

    Controle de envio de mensagens ASCII via UART Funções para envio de strings com terminação nula

--- portifolio_mic/Aula 6/main.c
+++ portifolio_mic/Aula 6/main.c
@@ -1,36 +1,52 @@
+
 /*
-*main.c
-*
-*Create: 09/17/2026 10:13:34 AM
-* Author: Gabriel Demossi P. Machado
-*/
+ * main.c
+ *
+ * Created: 9/24/2026 08:15:07 AM
+ *  Author: Gabriel Demossi Pereira Machado
+ */ 
 
 //#define F_CPU 16000000 
 #include <avr/io.h> 	//Mapeamento dos registradores || No microship studio 
-#include "util/delay.h"
+#include "util/delay.h"	
 
-// Configuração de modulo de paridade
+char gMessage[16] = "Mensagem"; //"Null terminated strings" (8 bytes + 1 terminação nula)
+
+/**
+ * Configuração do módulo USART0 para modo assíncrono, frame de 8 bits, paridade par, BAUD de 9600
+**/
 void UART_config() {
-	UCSR0A = (0<<U2X0);  // Ignorado  por enquanto 
+	UCSR0A = (0<<U2X0);							//Modo "double speed" desativado
 	UCSR0B = (0<<RXEN0)|(1<<TXEN0)|(0<<UCSZ02); // Habilita o transmissor UART0, frame de 8 bits
-	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00) // Modo assincrono
-				|(1<<UPM01)|(0<<UPM00) //Habilita paridade
-				|(0<<USBS0) //1 Bit de stop
-				|(1<<UCSZ01)|(1<<UCSZ00) //Frame de 8 bits
-				|(1<<UCPOL0); // Polaridade do clock: ignorada
-	UBRR0 = (103); //BAUD DE 9600(freq. de bits por seg.)ERRO DE 0.16%
+	UCSR0C = (0<<UMSEL01)|(0<<UMSEL00)			// Modo assíncrono
+		   | (1<<UPM01)|(0<<UPM00)				// Habilita paridade par
+		   | (1<<USBS0)							// 2 bits de stop
+		   | (1<<UCSZ01)|(1<<UCSZ00)			// Frame de 8 bits
+		   | (0<<UCPOL0);						// Polaridade do clock: ignorada
+	UBRR0 = 103; //BAUD de 9600, erro de 0,16%
 }
-// Envia um byte na USATR
 
-void UART_send(){
-	UDR0 = 0x53; //Envia umm byte exemplo
-	_delay_ms(2);
+/**
+ *	Envia um byte na USART0
+**/
+void UART_send_byte(uint8_t pByte) {
+	// Aguardar espaço no buffer (semáforo)
+	while((UCSR0A & (1<<UDRE0)) == 0);
+	UDR0 = pByte; //Envia o byte do parâmetro
 }
-int main(void)
-{
-	UART_config();
-	while(1) 	// Nunca retira esse while, pois o programa depende dele para funcionar
-	{
-		UART_send();
+
+void UART_send_string(char *pString) {
+	char *tMessagePtr = pString;
+	while(*tMessagePtr != 0) {
+		UART_send_byte(*tMessagePtr);
+		tMessagePtr++;
 	}
+}
+
+int main(void) {
+	UART_config();
+    while(1) {
+        UART_send_string(gMessage);
+		_delay_ms(5); //Tempo de silêncio
+    }
 }
